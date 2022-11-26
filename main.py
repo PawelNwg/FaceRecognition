d2cff9f7f8cf9cf7b1bb60dc26c6d3979fc2feaa
99sgabi <99sgabi@wp.pl>
2022-11-26 17:17:52 +0100

    added comments

--- main.py
+++ main.py
@@ -5,33 +5,29 @@ import mediapipe as mp
 import time as time
 import math
 
+#wytrenowane modele
 face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eyes = cv2.CascadeClassifier("haarcascade_eye.xml")
 mouth = cv2.CascadeClassifier("Mouth.xml")
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
 
-blur = 21
-canny_low = 15
-canny_high = 150
-min_area_g = 0.0005
-max_area_g = 0.95
-dilate_iter = 10
-erode_iter = 10
-mask_color = (0.0, 0.0, 0.0)
 
 segmentor = SelfiSegmentation()
 
 video_capture = cv2.VideoCapture(0)
 
-# Covers both range
+# zakres kolorow skory w hsv
 lower = np.array([0, 30, 53], dtype = "uint8")
 upper = np.array([20, 180, 255], dtype = "uint8")
 
 lower2 = np.array([172, 30, 53], dtype = "uint8")
 upper2 = np.array([180, 180, 210], dtype = "uint8")
 
+#wybrene id landmarkow do pomiaru predkosci
 landmarks_ids = [1, 15, 50, 280]
 
 def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text):
@@ -55,48 +51,49 @@ def detect(img, color):
 
     return img
 
-
-mpDraw = mp.solutions.drawing_utils
-mpFaceMesh = mp.solutions.face_mesh
-face_mesh = mpFaceMesh.FaceMesh()
-
+# pozycje wybranych landmarkow
 landmarks_prev_position = {landmarks_ids[0] : None, landmarks_ids[1]: None, landmarks_ids[2]: None, landmarks_ids[3]: None }
 landmarks_prev_time = {landmarks_ids[0] : None, landmarks_ids[1]: None, landmarks_ids[2]: None, landmarks_ids[3]: None }
 
 while True:
     ret, img = video_capture.read(0)
     image = img.copy()
+    # Rysowanie konturów
     if ret:
         image = segmentor.removeBG(image, (0, 0, 0))
-    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    imgInHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    skinMask = cv2.inRange(converted, lower, upper)
-    skinMask2 = cv2.inRange(converted, lower2, upper2)
+    mask1 = cv2.inRange(imgInHSV, lower, upper)
+    mask2 = cv2.inRange(imgInHSV, lower2, upper2)
 
-    #Gaussian Blur
-    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
-    skinMask2 = cv2.GaussianBlur(skinMask2, (3, 3), 0)
+    #Gaussian Blur - rozmycie gaussowskie - dodanie do maski
+    mask1 = cv2.GaussianBlur(mask1, (3, 3), 0)
+    mask2 = cv2.GaussianBlur(mask2, (3, 3), 0)
 
-    skin1 = cv2.bitwise_and(image, image, mask = skinMask)
-    skin2 = cv2.bitwise_and(image, image, mask = skinMask2)
-    skin = cv2.bitwise_or(skin1,skin2) #adding both ranges
+    imgWithSkin1 = cv2.bitwise_and(image, image, mask = mask1)
+    imgWithSkin2 = cv2.bitwise_and(image, image, mask = mask2)
 
-    img_gray = cv2.cvtColor(skin, cv2.COLOR_BGR2GRAY)
-    # apply binary thresholding
-    ret, thresh = cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY)
+    #wzięcie obu zakresów kolorów skóry
+    skinImg = cv2.bitwise_or(imgWithSkin1, imgWithSkin2)
 
-    # detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+    img_gray = cv2.cvtColor(skinImg, cv2.COLOR_BGR2GRAY)
+    # wybranie tylko tych kolorów skóry (wartosc mniejsza niz 50 jest ustawiane na 0)
+    ret, thresh = cv2.threshold(img_gray, 25, 255, cv2.THRESH_BINARY)
+
+    # rysowanie konturów na podstawie punktów
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-    # draw contours on the original image
-    image_copy = skin.copy()
+    # rysowanie kontur
+    # image_copy = skinImg.copy()
+    image_copy = img.copy()
     cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
                      lineType=cv2.LINE_AA)
 
-    # Writing processed image in a new window
+    # Wykrywanie oczu i ust
     img2 = img.copy()
     img2 = detect(img2, (255, 0, 0))
 
+    # Mierzenie predkosci
     image3 = img.copy()
     image3 = cv2.cvtColor(image3, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(image3)
@@ -111,7 +108,7 @@ while True:
                 landmark_drawing_spec=None,
                 connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style()
             )
-            for id,landmark in enumerate(face_landmarks.landmark):
+            for id, landmark in enumerate(face_landmarks.landmark):
                 ih, iw, ic = image3.shape
                 x, y = int(landmark.x*iw), int(landmark.y*ih)
                 if id in landmarks_ids:
@@ -124,7 +121,6 @@ while True:
                     landmarks_prev_position[id] = (x, y)
 
     cv2.imshow("Biometria II", np.hstack([np.vstack([img, image_copy]), np.vstack([image3, img2])]))
-    cv2.imshow("Biometria", np.vstack([image3, img2]))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
